ede417f5e8f2226ad974e130277107466859b62f
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-09-24 14:58:12 +0200

    Test

--- imagesEtGeom/geometrie.c
+++ imagesEtGeom/geometrie.c
@@ -181,9 +181,9 @@ void line(int i1, int j1, int i2, int j2, color c) {
         int copy=i2;
         i2=i1;
         i1=copy;
-	copy=j2;
-	j2=j1;
-	j1=copy;
+        copy=j2;
+        j2=j1;
+        j1=copy;
     }
     if (i2==i1) {
 	hline(i1,j1,j2,c);
