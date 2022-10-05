d52f4b0a993c1c9b8031b9b8d5c120c466eb890d
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-10-05 18:17:11 +0200

    Dynarray

---
+++ testsPerso/dynarray.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int *a;
+    int length;
+    scanf("Enter int value : %d",&length);
+    a=malloc(sizeof(int) * length);
+    printf("[");
+    for (int i=0;i<length;i++) {
+        a[i]=i+1;
+        printf("%d",a[i]);
+        if (i<length-1) {
+            printf(", ");
+        }
+    }
+    printf("]");
+    free(a);
+}
