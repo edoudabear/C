ec2cf0ac1220a44acfdc5229de2965ec2e818bc1
Edouard <44883032+edoudabear@users.noreply.github.com>
2022-10-06 14:25:59 +0200

    Premiers

--- premiers/premiers.c
+++ premiers/premiers.c
@@ -120,7 +120,7 @@ void gen_erathostene_2(void) {
     }
     for (int i=2;i<65535;i++) {
         if (is_prime[i]) {
-            for (int j=2*i;j<65535;j+=2*i) {
+            for (int j=2*i;j<65535;j+=i) {
                 is_prime[j]=false;
             }
         }
